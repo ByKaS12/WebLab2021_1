f1eacc3895f3f2d5ab62818b7f150504727fcf49
ByKaS12 <67284495+ByKaS12@users.noreply.github.com>
2021-04-25 23:26:47 +0300

    Update main.js
    Fix too many pictures for request api

--- main.js
+++ main.js
@@ -4,23 +4,31 @@ async function sendApiRequest() {
     let date = document.getElementById('date').value.toString()
     let count = document.getElementById('count').value.toString()
     let thumbs = document.getElementsByName('thumbs')[0].checked
-    let flag
-    let endUrl
-    if (count != 0) {
-        flag = 'count'
-        endUrl = url + '&hd=true&thumbs=' + thumbs + '&concept_tags=true&count=' + count
-    } else {
-        flag = 'date'
-        endUrl = url + '&hd=true&thumbs=' + thumbs + '&concept_tags=true&date=' + date
-    }
-    let response = await fetch(endUrl)
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
-    useApiData(data, flag)
+
+let flag
+let endUrl
+if (count != 0) {
+    flag = 'count'
+    endUrl = url + '&hd=true&thumbs=' + thumbs + '&concept_tags=true&count=' + count
+} else {
+    flag = 'date'
+    endUrl = url + '&hd=true&thumbs=' + thumbs + '&concept_tags=true&date=' + date
+}
+let response = await fetch(endUrl)
+console.log(response)
+let data = await response.json()
+console.log(data)
+useApiData(data, flag)
 }
 
 async function useApiData(data, flag) {
+    let checkMain = document.getElementsByClassName('Main')
+    if(checkMain[0]!=null){
+        document.body.removeChild(checkMain[0]);
+    }
+     let mainDiv = document.createElement('div');
+     mainDiv.className = "Main";
+     document.body.appendChild(mainDiv);
     if (flag == 'date') {
         if (data.media_type == 'image' || data.media_type == 'gif') {
             let url = data.url
@@ -28,12 +36,13 @@ async function useApiData(data, flag) {
             div.className = "Pictures";
             div.innerHTML = data.explanation
             var x = document.createElement("IMG");
+            x.className = 'Pics';
             x.setAttribute("src", url);
             x.setAttribute("width", "480");
             x.setAttribute("height", "240");
             x.setAttribute("alt", "NASA IMG");
-            document.body.appendChild(x);
-            document.body.appendChild(div);
+            mainDiv.appendChild(x);
+            mainDiv.appendChild(div);
 
         } else {
             let url = data.url
@@ -41,29 +50,31 @@ async function useApiData(data, flag) {
             div.className = "Pictures";
             div.innerHTML = data.explanation
             var x = document.createElement("iframe");
+            x.className = 'Pics';
             x.setAttribute("src", url);
             x.setAttribute("frameborder", '0');
             x.setAttribute("width", "480");
             x.setAttribute("height", "240");
-            document.body.appendChild(x);
-            document.body.appendChild(div);
+            mainDiv.appendChild(x);
+            mainDiv.appendChild(div);
         }
     } else {
-        data.forEach(function (item, i, data){
+        data.forEach(function (item, i, data) {
             if (data[i].media_type == 'image' || data[i].media_type == 'gif') {
                 let url = data[i].url
                 let div = document.createElement('div');
                 div.className = "Pictures";
                 div.innerHTML = data[i].explanation
                 var x = document.createElement("IMG");
+                x.className = 'Pics';
                 x.setAttribute("src", url);
                 x.setAttribute("width", "480");
                 x.setAttribute("height", "240");
                 x.setAttribute("alt", "NASA IMG");
-                document.body.appendChild(x);
-                document.body.appendChild(div);
-                document.body.appendChild(document.createElement("br"));
-                document.body.appendChild(document.createElement("hr"));
+                mainDiv.appendChild(x);
+                mainDiv.appendChild(div);
+                mainDiv.appendChild(document.createElement("br"));
+                mainDiv.appendChild(document.createElement("hr"));
 
             } else {
                 let url = data[i].url
@@ -71,12 +82,13 @@ async function useApiData(data, flag) {
                 div.className = "Pictures";
                 div.innerHTML = data[i].explanation
                 var x = document.createElement("iframe");
+                x.className = 'Pics';
                 x.setAttribute("src", url);
                 x.setAttribute("frameborder", '0');
                 x.setAttribute("width", "480");
                 x.setAttribute("height", "240");
-                document.body.appendChild(x);
-                document.body.appendChild(div);
+                mainDiv.appendChild(x);
+                mainDiv.appendChild(div);
             }
 
         })
